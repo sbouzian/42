7e645e31ed427f7a62e2001921c7e53d453aa3e3
sbouzian <salmabouzian42@gmail.com>
2026-01-31 22:10:23 +0100

    primera versión de las funciones, revisar funciones mem

---
+++ Libft/ft_memcpy.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void    *ft_memcpy(*dest, const void *src, size_t n)
+{
+    unsigned char   *d;
+    const unsigned char *s;
+    size_t  i;
+
+    d = (unsigned char *)dest;
+    s = (const unsigned char *)src;
+    i = 0;
+    while (i < n)
+    {
+        d[i] = s[i];
+        i++;
+    }
+    return (dest);
+}
